160397cebc3056da9abe35ccdd7c0c40e2e5c007
jdromero88 <jdromerov88@gmail.com>
2020-08-26 02:35:07 -0400

    Login form

---
+++ src/clickcaja/ClickCaja.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clickcaja;
+
+import view.Login;
+
+/**
+ *
+ * @author jodarove
+ */
+public class ClickCaja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login open = new Login();
+        open.setVisible(true);
+    }
+    
+}
---
+++ src/controller/Usuario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+/**
+ *
+ * @author jodarove
+ */
+public class Usuario {
+    boolean login = false;
+    public boolean loguearUsuario(String usuario, String password){
+        if (usuario.isEmpty() || password.isEmpty()) {
+            login = false;
+            System.err.println("Completa los campos");
+        } else {
+            if ("admin".equals(usuario)) {
+                if ("admin".equals(password)) {
+                    login = true;
+                }else{
+                    login = false;
+                    System.err.println("Datos incorrectos.");
+                }  
+            }else{
+                login = false;
+                System.err.println("Datos Incorrectos.");
+            }
+        }
+        return login;
+    }
+    boolean cerrarSesion(){
+        return true;
+    }
+    public void salir(){
+        System.exit(0);;
+//        return true;
+    }
+}
