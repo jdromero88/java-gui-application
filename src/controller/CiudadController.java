4e40a7aa3e49309eecd1982bf22a6e268f1abf9a
jdromero88 <jdromerov88@gmail.com>
2020-09-17 01:33:55 -0400

    Updated Ciudad controller

--- src/controller/CiudadController.java
+++ src/controller/CiudadController.java
@@ -6,10 +6,61 @@
 
 package controller;
 
+import database.DBUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import model.Ciudad;
+
 /**
  *
  * @author jodarove
  */
 public class CiudadController {
+    // Traemos todas las ciudades y lo asignamos a un arraylist como objetos.
+    public static ArrayList<Ciudad> getAll() throws Exception{
+        String query = "SELECT * FROM ciudads ORDER by id";
+        ArrayList<Ciudad> ciudades = new ArrayList<>();
+        Connection connection = DBUtil.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery()){
+            while (rs.next()) {
+                Ciudad ciudad = new Ciudad();
+                ciudad.setId(rs.getInt("id"));
+                ciudad.setNombre(rs.getString("nombre"));
+                ciudad.setDepartamentoId(rs.getInt("departamento_id"));
+            }
+            return ciudades;
+        } catch (Exception e) {
+            System.err.println("Algo paso al querer traer todos las ciudades: " + e);
+            throw e;
+        } finally{
+            DBUtil.closeConnection();
+        }
+    }
     
+    public static Ciudad get(int id) throws Exception{
+        String query = "SELECT * FROM ciudads WHERE id = ?";
+        Connection connection = DBUtil.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(query)){
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Ciudad ciudad = new Ciudad();
+                ciudad.setId(rs.getInt("id"));
+                ciudad.setNombre(rs.getString("nombre"));
+                ciudad.setDepartamentoId(rs.getInt("departamento_id"));
+                return ciudad;
+            }else{
+                rs.close();
+                return null;
+            }
+        } catch (Exception e) {
+            System.err.println("Algo paso al hacer el get Ciudad: " + e);
+            throw e;
+        } finally{
+            DBUtil.closeConnection();
+        }
+    }
 }
--- src/model/Ciudad.java
+++ src/model/Ciudad.java
@@ -11,19 +11,40 @@ package model;
  * @author jodarove
  */
 public class Ciudad {
+    private int id;
     private String nombre;
     private int departamentoId;
     public Ciudad() {
     }
-    
-    public Ciudad(String nombre) {
+
+    public Ciudad(int id, String nombre, int departamentoId) {
+        this.id = id;
         this.nombre = nombre;
+        this.departamentoId = departamentoId;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }    
+    }
+
+    public int getDepartamentoId() {
+        return departamentoId;
+    }
+
+    public void setDepartamentoId(int departamentoId) {
+        this.departamentoId = departamentoId;
+    }
+        
 }
